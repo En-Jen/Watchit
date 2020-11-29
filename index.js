63315b11883a4cf703f8bfccd6c5c7f0a9e19964
Jen Speak <jspeak6@gmail.com>
2020-11-29 13:27:54 -0800

    added chokidar & lodash.debounce packages

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('i was executed');
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+
+// start function has to wait 100ms without a new 'add' before being triggered
+const start = debounce(() => {
+    console.log('STARTING USERS PROGRAM');
+}, 100);
+
+// Watches for file additions, changes, and deletions
+chokidar
+    .watch('.')
+    .on('add', start)
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlink', () => console.log('FILE UNLINKED'));
+
